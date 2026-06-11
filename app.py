10523ae188ec7f2d603d3b13e37bd9bdd807f58b
McMahon <1608586721121005@mil>
2026-06-11 05:09:13 -0500

    archive shop dropdown fix 2

--- app.py
+++ app.py
@@ -2282,7 +2282,6 @@ def archive():
         group_keys=GROUP_KEYS,
         shop_users=shop_users,                # ← filtered users
         available_groups=available_groups,    # ← filtered groups
-        **_topbar_context(),
     )
 
 
